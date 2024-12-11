0f70639c080a845f7c87b406de210d875064e286
kaueOliveira <143552040+kaueOliveira@users.noreply.github.com>
2024-12-11 18:01:26 -0300

    Melhorias no código

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -1,7 +1,6 @@
 const btn = document.querySelectorAll(".btn_add");
-//const main = document.getElementsByTagName("main")[0]
-const section_cart = document.getElementById("cart")
-const msg_quantity = section_cart.getElementsByTagName("h1")[0]
+const section_cart = document.getElementById("cart");
+const msg_quantity = section_cart.getElementsByTagName("h1")[0];
 
 const items = [
     {name: "Waffle with Berries", price: 6.50},
@@ -14,17 +13,15 @@ const items = [
     {name: "Salted Caramel Brownie", price: 4.50},
     {name: "Vanilla Panna Cotta", price: 6.50},
 ]
-
-let intervalo;
-
-let pedido_total2 = 0
-let quantity_items_cart = 0
-
-let has_msg_empty = true
-let order_made = false
+// criar objeto nessas variaveis
+let interval;
+let total_order_value = 0;
+let quantity_items_cart = 0;
+let has_msg_empty = true;
+let order_made = false;
 
 btn.forEach( b => {
-    let quantity = 0
+    let quantity = 0;
     b.addEventListener("click", (evt) => {
         const img_product = b.parentNode.children[0] // img do carrinho
         
@@ -33,7 +30,7 @@ btn.forEach( b => {
         img_cart.setAttribute("alt", "add to cart icon");
         
         if (b.classList.value === "btn_add") {
-            quantity ++
+            quantity ++;
 
             const img_decrement = document.createElement("img"); // Botão de remover 1
             img_decrement.setAttribute("src", "assets/images/icon-decrement-quantity.svg");
@@ -52,24 +49,23 @@ btn.forEach( b => {
             b.append(quantity);
             b.appendChild(img_increment);
 
-            img_product.classList.add("selected") // Borda ao redor da imagem
+            img_product.classList.add("selected");// Borda ao redor da imagem
 
             // Adicionando no Carrinho
-            let chosen_item_name = b.parentNode.children[2].innerHTML
-                
-            let chosen_item_price =  b.parentNode.children[3].innerHTML
+            let chosen_item_name = b.parentNode.children[2].innerHTML;   
+            let chosen_item_price =  b.parentNode.children[3].innerHTML;
                 
             items.forEach(i => {
                 if(i.name === chosen_item_name) {
-                    chosen_item_name = i.name
-                    chosen_item_price = i.price
+                    chosen_item_name = i.name;
+                    chosen_item_price = i.price;
                 }
-            })
+            });
 
             // Remove o carrinho vazio
             if (has_msg_empty) {
-                section_cart.children[1].remove(), section_cart.children[1].remove()
-                has_msg_empty = false
+                section_cart.children[1].remove(), section_cart.children[1].remove();
+                has_msg_empty = false;
             }
             
             // Cria a seção do item
@@ -90,9 +86,10 @@ btn.forEach( b => {
             itemHeader.appendChild(removeIcon);
             
             const itemDetails = document.createElement("div");
+            itemDetails.id = "details"
             
             const itemQuantity = document.createElement("p");
-            itemQuantity.id = "amount"
+            itemQuantity.id = "amount";
             itemQuantity.textContent = quantity + "x";
             
             const itemPrice = document.createElement("p");
@@ -101,7 +98,7 @@ btn.forEach( b => {
             
             const itemTotal = document.createElement("p"); 
             itemTotal.textContent = "$" + quantity * chosen_item_price;
-            itemTotal.id = "total"
+            itemTotal.id = "total";
             
             itemDetails.appendChild(itemQuantity);
             itemDetails.appendChild(itemPrice);
@@ -111,65 +108,63 @@ btn.forEach( b => {
             itemSection.appendChild(itemDetails);
         
             section_cart.appendChild(itemSection);
-            quantity_items_cart++
-            msg_quantity.innerHTML = `Your Cart (${quantity_items_cart})`
+            quantity_items_cart++;
+            msg_quantity.innerHTML = `Your Cart (${quantity_items_cart})`;
 
-            pedido_total2 += quantity * chosen_item_price
-
-            funcao_confirm_order(chosen_item_name, chosen_item_price, quantity)
+            total_order_value += quantity * chosen_item_price;
             
             if (order_made) {
-                section_cart.children.confirm_order.remove()
+                section_cart.children.confirm_order.remove();
             }
 
-            const order_total = document.createElement("div")
-            order_total.id = "order_total"
+            const order_total = document.createElement("div");
+            order_total.id = "order_total";
 
-            const p1 = document.createElement("p")
-            p1.textContent = "Order Total"
+            const p1 = document.createElement("p");
+            p1.textContent = "Order Total";
     
-            const p_total_price = document.createElement("p")
-            p_total_price.id = "total_price"
+            const p_total_price = document.createElement("p");
+            p_total_price.id = "total_price";
                     
-            intervalo = setInterval(() => {
-                p_total_price.textContent = "$ " + pedido_total2
+            interval = setInterval(() => {
+                p_total_price.textContent = "$ " + total_order_value;
             }, 500);
                       
-            order_total.appendChild(p1)
-            order_total.appendChild(p_total_price)
+            order_total.appendChild(p1);
+            order_total.appendChild(p_total_price);
 
-            const p_img = document.createElement("p")
-            p_img.id = "p_img"
+            const p_img = document.createElement("p");
+            p_img.id = "p_img";
 
-            const img = document.createElement("img")
-            img.id = "img_carbon"
-            img.src = "assets/images/icon-carbon-neutral.svg"
+            const img = document.createElement("img");
+            img.id = "img_carbon";
+            img.src = "assets/images/icon-carbon-neutral.svg";
 
-            p_img.appendChild(img)
+            p_img.appendChild(img);
 
-            p_img.innerHTML += " This is a carbon-neutral delivery"
+            p_img.innerHTML += " This is a carbon-neutral delivery";
 
-            finalizar_pedido(order_total, p_img)
+            complete_order(order_total, p_img); // X
 
             //Removendo e Adicionando itens
             img_decrement.addEventListener("click", (evt) => {
-                quantity--
-                b.childNodes[1].nodeValue = quantity
+                quantity--;
+                b.childNodes[1].nodeValue = quantity; //atualizando numero de itens escolhidos
                 itemQuantity.textContent = quantity + "x";
                 itemTotal.textContent = "$" + quantity * chosen_item_price;
-                pedido_total2 = pedido_total2 - chosen_item_price
+                total_order_value = total_order_value - chosen_item_price; 
                 if (quantity === 0) {
-                    p_total_price.textContent = "$ " + pedido_total2
-                    itemSection.remove()
+                    p_total_price.textContent = "$ " + total_order_value;
+                    itemSection.remove();
                 }
             });
 
             img_increment.addEventListener("click", () => {
                 quantity++
-                b.childNodes[1].nodeValue = quantity
+                b.childNodes[1].nodeValue = quantity;
                 itemQuantity.textContent = quantity + "x";
                 itemTotal.textContent = "$" + quantity * chosen_item_price;
-                pedido_total2 += chosen_item_price
+                total_order_value += chosen_item_price;
             });
         } 
 
@@ -181,99 +176,89 @@ btn.forEach( b => {
             b.append(" Add to Cart");
 
             img_product.classList.remove("selected")
-            quantity_items_cart--
-            msg_quantity.innerHTML = `Your Cart (${quantity_items_cart})`
-            let nome = b.parentNode.children[2].innerHTML
-            removendo_item_especifico(nome)
-
+            quantity_items_cart--;
+            msg_quantity.innerHTML = `Your Cart (${quantity_items_cart})`;
+            let nome = b.parentNode.children[2].innerHTML;
+            remove_item_from_order_confirmation(nome);
         };
 
         if (quantity_items_cart === 0) { // carrinho vazio
-            cart_empty ()
-            clearInterval(intervalo);
-            section_cart.children.confirm_order.remove()
-            has_msg_empty = true
-            order_made =  false
+            cart_empty ();
+            clearInterval(interval);
+            section_cart.children.confirm_order.remove();
+            has_msg_empty = true;
+            order_made =  false;
         }
-    })
-}) 
+    });
+});
 
 function cart_empty () {
-    const div_cart_empty = document.createElement("div")
-    div_cart_empty.className = "cart_empty"
+    const div_cart_empty = document.createElement("div");
+    div_cart_empty.className = "cart_empty";
 
-    const p_empty = document.createElement("p")
-    p_empty.textContent = "Your added items will appear here"
+    const p_empty = document.createElement("p");
+    p_empty.textContent = "Your added items will appear here";
 
-    section_cart.appendChild(div_cart_empty)
-    section_cart.appendChild(p_empty)
+    section_cart.appendChild(div_cart_empty);
+    section_cart.appendChild(p_empty);
 }
 
-function funcao_confirm_order (chosen_item_name, chosen_item_price, quantity) {
-    
-    const msg_box_orders = document.getElementById("msg_box_orders"); // X
-    
-    const itemSection2 = document.createElement("section");
-    itemSection2.className = "order";
-    
-    const itemHeader = document.createElement("div");
-    
-    const itemTitle = document.createElement("h1");
-    itemTitle.textContent = chosen_item_name;
-    
-    itemHeader.appendChild(itemTitle);
-    
-    const itemDetails = document.createElement("div");
-    itemDetails.id = "details"
-    
-    const itemQuantity = document.createElement("p");
-    itemQuantity.id = "amount"
-    itemQuantity.textContent = quantity + "x";
-    
-    const itemPrice = document.createElement("p");
-    itemPrice.id = "price"
-    itemPrice.textContent = "@ $"+ chosen_item_price;
-    
-    const itemTotal = document.createElement("p"); 
-    itemTotal.textContent = "$" + quantity * chosen_item_price;
-    itemTotal.id = "total"
-    
-    itemDetails.appendChild(itemQuantity);
-    itemDetails.appendChild(itemPrice);
-    itemDetails.appendChild(itemTotal);
-    
-    itemSection2.appendChild(itemHeader);
-    itemSection2.appendChild(itemDetails);
-    
+function adding_items_to_order_confirmation () {
+    const msg_box_orders = document.getElementById("msg_box_orders");
+    const pedidos = [...document.getElementsByClassName("order")];
 
-    msg_box_orders.appendChild(itemSection2);
+    pedidos.forEach(pedido => {
+        const btn_remove = document.getElementById("remove");
+        btn_remove.remove();
+        msg_box_orders.appendChild(pedido);
+    });
 }
 
-function finalizar_pedido (order_total, p_img) {
-    const btn_confirm_order = document.createElement("div")
-        btn_confirm_order.id = "btn_confirm_order"
-        btn_confirm_order.innerHTML = "Confirm Order"
+function complete_order (order_total, p_img) { //Finaliza o pedido e cria caixas.
+    const btn_confirm_order = document.createElement("div");
+        btn_confirm_order.id = "btn_confirm_order";
+        btn_confirm_order.innerHTML = "Confirm Order";
 
-        const section_confirm_order = document.createElement("section")
-        section_confirm_order.id = "confirm_order"
+        const section_confirm_order = document.createElement("section");
+        section_confirm_order.id = "confirm_order";
 
-        section_confirm_order.appendChild(order_total)
-        section_confirm_order.append(p_img)
-        section_confirm_order.append(btn_confirm_order)
+        section_confirm_order.appendChild(order_total);
+        section_confirm_order.append(p_img);
+        section_confirm_order.append(btn_confirm_order);
 
-        section_cart.appendChild(section_confirm_order)
+        section_cart.appendChild(section_confirm_order);
 
-        order_made = true
+        order_made = true;
             
         btn_confirm_order.addEventListener("click", (evt)=>{
-            const div_backGroung = document.getElementById("back_ground")
-            const msg_box = document.getElementById("msg_box")
+            adding_items_to_order_confirmation ()
+            const div_background = document.getElementById("back_ground");
+            const msg_box = document.getElementById("msg_box");
 
-            div_backGroung.style.width = "100%"
-            div_backGroung.style.height = "200vh"
+            div_background.style.width = "100%";
 
-            msg_box.style.width = "450px"
-            msg_box.style.overflow = "auto"   
+            window.addEventListener("resize", ()=>{
+                console.log(window.innerWidth);
+                if(window.innerWidth < "650") {
+                    div_background.style.height = "100vh";
+                } else {
+                    let tamanho_tela = 276400 / window.innerWidth;
+                    div_background.style.height = `${tamanho_tela}vh`;
+                }
+            });
+            
+            if(window.innerWidth < "650") {
+                div_background.style.height = "100vh";
+            } else {
+                let tamanho_tela = 276400 / window.innerWidth;
+                div_background.style.height = `${tamanho_tela}vh`;
+            }
+            
+            let tamanho_tela = 276400 / window.innerWidth;
+            div_background.style.height = `${tamanho_tela}vh`;
+
+            msg_box.style.width = "400px";//mudar p/ media-query
+            msg_box.style.overflow = "auto";
                 
             const order_total_msg_box = document.getElementById("msg_box_totalOrder");
 
@@ -281,42 +266,42 @@ function finalizar_pedido (order_total, p_img) {
             p_order_total.innerHTML = "Order Total";
             p_order_total.id = " p_order_total";
 
-            const pTotalValue = document.createElement("p")
-            pTotalValue.innerHTML = "$ " + pedido_total2;
-            pTotalValue.id = "p_total_value"
+            const pTotalValue = document.createElement("p");
+            pTotalValue.innerHTML = "$ " + total_order_value;
+            pTotalValue.id = "p_total_value";
 
-            order_total_msg_box.appendChild(p_order_total)
-            order_total_msg_box.appendChild(pTotalValue)
+            order_total_msg_box.appendChild(p_order_total);
+            order_total_msg_box.appendChild(pTotalValue);
 
                 
-            const btn_new_order = document.getElementById("btn_new_order")
+            const btn_new_order = document.getElementById("btn_new_order");
             btn_new_order.addEventListener("click", (evt)=>{
-                div_backGroung.style.width = "0px"
-                div_backGroung.style.height = "0vh"
+                div_background.style.width = "0px";
+                div_background.style.height = "0vh";
 
-                msg_box.style.width = "0px"
-                msg_box.style.overflow = "hidden"
+                msg_box.style.width = "0px";
+                msg_box.style.overflow = "hidden";
 
-                p_order_total.remove()
-                pTotalValue.remove()
+                p_order_total.remove();
+                pTotalValue.remove();
                     
-                evt.target.parentNode.children[1].children[0].remove()
+                evt.target.parentNode.children[1].children[0].remove();
 
-                location.reload()
+                location.reload();
                 //reset() 
             });
-    })
+    });
 }
 
-function removendo_item_especifico (nome) {
-    const pedido = [...msg_box_orders.getElementsByClassName("order")]
+function remove_item_from_order_confirmation (nome) {
+    const pedido = [...msg_box_orders.getElementsByClassName("order")];
 
     pedido.map(p => {
         let nome_box = p.children[0].children[0].innerHTML
         if (nome === nome_box) {
-            p.remove()
+            p.remove();
         }
-    })
+    });
 }
 
 function reset () { //ERRO
@@ -330,3 +315,6 @@ function reset () { //ERRO
     // has_msg_empty = true
     // order_made =  false    
 }
+
+
+// 1536 ------ 180 = 276480
