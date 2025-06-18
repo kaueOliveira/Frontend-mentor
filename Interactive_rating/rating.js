8d8b0ad03f0579368d0c25098d8c175cac8a691a
Kauê Oliveira <kaueoliveira993@gmail.com>
2025-06-18 19:26:36 -0300

    Refatorando Código

--- Interactive_rating/rating.js
+++ Interactive_rating/rating.js
@@ -1,67 +1,33 @@
-const btn = [...document.querySelectorAll(".btn")]
-const msg = document.getElementById("msg")
-const enviar = document.getElementById("enviar")
-let elo
+const buttons = document.querySelectorAll(".button");
+const buttonSubmit = document.getElementById("submit-button");
 
-const tirarSelecao = () => {
-    const cursoSelecionados = [...document.querySelectorAll(".selecionado")]
-    cursoSelecionados.map((el) => {
-        el.classList.remove("selecionado")
-    })
-}
+const containerMain = document.getElementById("container");
+const containerTy = document.getElementById("container-thank-you");
 
-btn.map((el) => {
-    el.addEventListener("click", (evt) => {
-        tirarSelecao()
-        const el = evt.target
-        el.classList.toggle("selecionado")
+const mensage = document.getElementById("mensage");
 
-    })
-})
-
-btn.map((el) => {
-    el.addEventListener("mouseover", (evt) => {
-        const el = evt.target
-        el.classList.toggle("hoo")
-    })
-})
-
-btn.map((el) => {
-    el.addEventListener("mouseout", (evt) => {
-        const el = evt.target
-        el.classList.remove("hoo")
-    })
-})
-
-
-enviar.addEventListener("click", function() {
-    btn.forEach((el, i) => {
-        i ++
-        if(el.classList.contains("selecionado")) {
-            window.location.href = `pagina${i}/form${i}.html`;
-        }
-    });
+buttons.forEach((button) => {
+  button.addEventListener("click", (evt) => {
+    const clickedButton = evt.target;
+    checkClasses();
+    clickedButton.classList.add("clicked");
+    submit(button.innerHTML)
+  });
 });
 
-// let c
-// enviar.addEventListener("click", function() {
-//     btn.map((el, i) => {
-//         c++
-//         if(el.classList.value == "selecionado") {
-//             window.location.href = `pagina${i}/form${i}.html`
-//         }
-//     })
-// })
+function checkClasses() {
+  buttons.forEach((button) => {
+    if (button.classList.contains("clicked")) {
+      button.classList.remove("clicked");
+    }
+  });
+}
 
-// let elo;
+function submit(number) {
+  buttonSubmit.addEventListener("click", (evt) => {
+    containerMain.style.display = "none";
+    containerTy.style.display = "block";
 
-// function sla() {
-//     btn.map((el) => {
-//         el.addEventListener("click", (evt) => {
-//              elo = evt.target.innerHTML
-//         })
-//     })
-// }
-
-// console.log(elo)
-// msg.innerHTML = `<p>You selected ${elo} out of 5</p>`
+    mensage.textContent = ` You selected ${number} out of 5`;
+  });
+}
