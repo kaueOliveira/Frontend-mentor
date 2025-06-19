851a2aebb41986a1df421970bfec814402dbd3af
Kauê Oliveira <kaueoliveira993@gmail.com>
2025-06-19 19:22:20 -0300

    Refatorando Código

--- faq_accordion/index.js
+++ faq_accordion/index.js
@@ -1,52 +1,24 @@
-const botao = document.querySelectorAll(".btn")
+const buttons = document.querySelectorAll(".button");
 
-botao.forEach(btn => {
-    btn.addEventListener("click", (evt)=>{
-        const elemento = evt.target;
-        const div_pai = evt.target.parentNode.parentNode
-        const contentDiv = div_pai.querySelector('.div_inativa');
-        const isITminus = elemento.src.includes('icon-minus.svg');
+buttons.forEach((button) => {
+  button.addEventListener("click", (evt) => {
+    const clickedButton = evt.target;
+    
+    checkSrc();
 
-        function Ocultar () {
-            elemento.src = 'assets/images/icon-plus.svg'
-            elemento.classList = 'btn_minus'
-            contentDiv.style.height = '0'; // Oculta o conteúdo
-            contentDiv.style.overflow = 'hidden';
-        }
+    clickedButton.src = "assets/images/icon-minus.svg";
+    clickedButton.parentNode.parentNode.children[1].style.overflow = "auto";
+    clickedButton.parentNode.parentNode.children[1].style.height = "9.5rem";
+  });
+});
 
-        let ativo = document.getElementById("ativo")
-        
-        function remover () {
-            let img = ativo.parentNode.children[0].children[1]
-            img.src = 'assets/images/icon-plus.svg'
-            ativo.style.height = '0'; 
-            ativo.style.overflow = 'hidden';
-            ativo.removeAttribute('id')
-        }
-        
-        if(isITminus) {
-            Ocultar()
-        }
-
-        if(!isITminus) {
-            if (ativo == null) {
-                elemento.src = 'assets/images/icon-minus.svg'
-                elemento.classList = 'btn'
-
-                div_pai.children[1].style.height = "auto" 
-                div_pai.children[1].style.overflow = "visible" 
-                div_pai.children[1].id = "ativo"
-            } else {
-                remover()
-                elemento.src = 'assets/images/icon-minus.svg'
-                elemento.classList = 'btn'
-
-                div_pai.children[1].style.height = "auto" 
-                div_pai.children[1].style.overflow = "visible" 
-                div_pai.children[1].id = "ativo"
-            }
-        }
-    })
-})
-
-//Adicionar "animação"
+function checkSrc() {
+  buttons.forEach((button) => {
+    const imageSrc = button.getAttribute("src");
+    if (imageSrc == "assets/images/icon-minus.svg") {
+      button.src = "assets/images/icon-plus.svg";
+      button.parentNode.parentNode.children[1].style.overflow = "hidden";
+      button.parentNode.parentNode.children[1].style.height = "0rem";
+    }
+  });
+}
