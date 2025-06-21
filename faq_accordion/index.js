dea18850811966df13d0852dd6440acb0ad07bcd
Kauê Oliveira <kaueoliveira993@gmail.com>
2025-06-21 13:27:06 -0300

    Refatorando Código

--- faq_accordion/index.js
+++ faq_accordion/index.js
@@ -1,24 +1,32 @@
 const buttons = document.querySelectorAll(".button");
+const hiddenDivs = document.querySelectorAll(".hidden-div");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (evt) => {
-    const clickedButton = evt.target;
-    
-    checkSrc();
+buttons.forEach((button, btnIndex) => {
+  button.addEventListener("click", () => {
+    hiddenDivs.forEach((div, divIndex) => {
+      const divButton = buttons[divIndex]; // 1-) 
 
-    clickedButton.src = "assets/images/icon-minus.svg";
-    clickedButton.parentNode.parentNode.children[1].style.overflow = "auto";
-    clickedButton.parentNode.parentNode.children[1].style.height = "9.5rem";
+      if (btnIndex === divIndex) { // 2-) 
+        const isOpen = div.style.height && div.style.height !== "0rem";
+
+        if (isOpen) {
+          div.style.height = "0rem";
+          divButton.src = "assets/images/icon-plus.svg";
+        } else {
+          div.style.height = div.scrollHeight + "px";
+          divButton.src = "assets/images/icon-minus.svg";
+        }
+      } else { // 3-)
+        div.style.height = "0rem";
+        divButton.src = "assets/images/icon-plus.svg";
+      }
+    });
   });
 });
 
-function checkSrc() {
-  buttons.forEach((button) => {
-    const imageSrc = button.getAttribute("src");
-    if (imageSrc == "assets/images/icon-minus.svg") {
-      button.src = "assets/images/icon-plus.svg";
-      button.parentNode.parentNode.children[1].style.overflow = "hidden";
-      button.parentNode.parentNode.children[1].style.height = "0rem";
-    }
-  });
-}
+
+//botão que corresponde a div !
+
+//A div atual é a que o usuário clicou ?
+
+//Se for qualquer outra div (não a clicada), Fecha. Isso é o que garante que apenas uma resposta fique aberta por vez.
